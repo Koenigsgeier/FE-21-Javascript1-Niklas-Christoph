02fbaac091d21993676b74fbfc53a23bd69dbac3
Niklas <nschmies@web.de>
2021-06-23 14:34:46 +0200

    advanced done

---
+++ advanced.js
@@ -0,0 +1,29 @@
+var a = 7;
+
+var b = 14;
+
+var c = 21;
+
+var d = 36;
+
+var e = 42;
+
+let addition = a + b + c + d + e;
+console.log(addition);
+
+var f = 1;
+
+var g = 15;
+
+var h = 8;
+
+var i = 1;
+
+let multiplication = f*g*h*i;
+console.log(multiplication)
+
+let result = addition/multiplication;
+document.write(result)
+
+let multidimensional = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+console.log(multidimensional[1][1], multidimensional [4][2],multidimensional[5][3], multidimensional[2][3], multidimensional[2][1]);
